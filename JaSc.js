1b04b9d718f417d7f0eb14e9941ec58dca43225d
u46cm8 <touch2who@gmail.com>
2022-09-21 18:25:07 +0100

    first working iteration of the program, there remain more modifications to get the prog to fit the criteria

---
+++ JaSc.js
@@ -0,0 +1,62 @@
+
+let choices = ["Rock", "Paper", "Scissor"]
+function getComputerChoice(){
+    let compChoice = Math.floor(Math.random()*(2 - 0 + 1) + 0);
+    //console.log(choices[compChoice]);
+    return choices[compChoice];
+    
+}
+
+
+function letsPlay(){
+    
+    let done = false;
+    do{
+        //Geting users choice
+        let hChoice = prompt("Please enter your choice");
+        let cChoice = getComputerChoice();
+        let hU = hChoice.toUpperCase();
+        let cU = cChoice.toString().toUpperCase();
+        //console.log(cU);
+        if (cU =="ROCK"){
+            if(hU =="PAPER"){
+                console.log(`You win, your ${hChoice} convers the computer's ${cChoice}`);
+                done = true;
+            }
+            else if(hU =="SCISSOR"){
+                console.log(`You lose, your ${hChoice} is broken by the computer's ${cChoice}`);
+                done = true;
+            }
+        }
+        else if (cU =="PAPER"){
+            if(hU == "SCISSOR"){
+                console.log(`You win, your ${hChoice} cuts the computer's ${cChoice}`);
+                done = true;
+            }
+            else if(hU =="ROCK"){
+                console.log(`You lose, your ${hChoice} is covered by the computer's ${cChoice}`);
+                done = true;
+            }
+        }
+        else if (cU == "SCISSOR"){
+            if(hU =="ROCK"){
+                console.log(`You win, your ${hChoice} breaks the computer's ${cChoice}`);
+                done = true;
+            }
+            else if(hU == "PAPER"){
+                console.log(`You lose, your ${hChoice} is cut by the computer's ${cChoice}`);
+                done = true;
+            }
+            
+        }
+        else if(hU === "STOP"){
+            done = true;
+        }
+        if(hU === cU){
+            alert("Please try again your choice you choose the same as the computer");
+        }
+    }while(done == false)
+}
+
+
+letsPlay();
