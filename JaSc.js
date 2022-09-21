637d52a24551bf14d3d79c1cb076f3da123b211d
u46cm8 <touch2who@gmail.com>
2022-09-21 21:17:39 +0100

    Removed the while loop as it was not needed and new function will be used

--- JaSc.js
+++ JaSc.js
@@ -2,61 +2,63 @@
 let choices = ["Rock", "Paper", "Scissor"]
 function getComputerChoice(){
     let compChoice = Math.floor(Math.random()*(2 - 0 + 1) + 0);
-    //console.log(choices[compChoice]);
+
     return choices[compChoice];
     
 }
+//Geting users choice
+let hChoice = prompt("Please enter your choice");
+let cChoice = getComputerChoice();
 
 
-function letsPlay(){
+function playerSelection(hChoice, cChoice){
     
     let done = false;
-    do{
-        //Geting users choice
-        let hChoice = prompt("Please enter your choice");
-        let cChoice = getComputerChoice();
-        let hU = hChoice.toUpperCase();
-        let cU = cChoice.toString().toUpperCase();
-        //console.log(cU);
-        if (cU =="ROCK"){
-            if(hU =="PAPER"){
-                console.log(`You win, your ${hChoice} convers the computer's ${cChoice}`);
-                done = true;
-            }
-            else if(hU =="SCISSOR"){
-                console.log(`You lose, your ${hChoice} is broken by the computer's ${cChoice}`);
-                done = true;
-            }
-        }
-        else if (cU =="PAPER"){
-            if(hU == "SCISSOR"){
-                console.log(`You win, your ${hChoice} cuts the computer's ${cChoice}`);
-                done = true;
-            }
-            else if(hU =="ROCK"){
-                console.log(`You lose, your ${hChoice} is covered by the computer's ${cChoice}`);
-                done = true;
-            }
-        }
-        else if (cU == "SCISSOR"){
-            if(hU =="ROCK"){
-                console.log(`You win, your ${hChoice} breaks the computer's ${cChoice}`);
-                done = true;
-            }
-            else if(hU == "PAPER"){
-                console.log(`You lose, your ${hChoice} is cut by the computer's ${cChoice}`);
-                done = true;
-            }
-            
-        }
-        else if(hU === "STOP"){
+
+
+    let hU = hChoice.toString().toUpperCase();
+    let cU = cChoice.toString().toUpperCase();
+    console.log(cU);
+    if (cU =="ROCK"){
+        if(hU =="PAPER"){
+            console.log(`You win, your ${hChoice} convers the computer's ${cChoice}`);
             done = true;
         }
-        if(hU === cU){
-            alert("Please try again your choice you choose the same as the computer");
+        else if(hU =="SCISSOR"){
+            console.log(`You lose, your ${hChoice} is broken by the computer's ${cChoice}`);
+            done = true;
         }
-    }while(done == false)
+    }
+    else if (cU =="PAPER"){
+        if(hU == "SCISSOR"){
+            console.log(`You win, your ${hChoice} cuts the computer's ${cChoice}`);
+            done = true;
+        }
+        else if(hU =="ROCK"){
+            console.log(`You lose, your ${hChoice} is covered by the computer's ${cChoice}`);
+            done = true;
+        }
+    }
+    else if (cU == "SCISSOR"){
+        if(hU =="ROCK"){
+            console.log(`You win, your ${hChoice} breaks the computer's ${cChoice}`);
+            done = true;
+        }
+        else if(hU == "PAPER"){
+            console.log(`You lose, your ${hChoice} is cut by the computer's ${cChoice}`);
+            done = true;
+        }
+        
+    }
+    else if(hU === "STOP"){
+        done = true;
+    }
+    if(hU === cU){
+        alert("Please try again your choice you choose the same as the computer");
+        done = true;
+    }
+    
 }
 
 
-letsPlay();
+playerSelection(hChoice, cChoice);
