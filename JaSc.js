50c7eddb0fe247a2141ad8f16716f46c06cd4ada
u46cm8 <touch2who@gmail.com>
2022-09-23 23:26:08 +0100

    the program is working in the console, UI might be added after

--- JaSc.js
+++ JaSc.js
@@ -6,46 +6,50 @@ function getComputerChoice(){
     return choices[compChoice];
     
 }
-//Geting users choice
-let hChoice = prompt("Please enter your choice");
-let cChoice = getComputerChoice();
 
 
-function playerSelection(hChoice, cChoice){
+
+function playerRound(hChoice, cChoice){
     
     let done = false;
 
 
     let hU = hChoice.toString().toUpperCase();
     let cU = cChoice.toString().toUpperCase();
-    console.log(cU);
+    console.log(`The computer's ${cChoice} vs your ${hChoice}`);
     if (cU =="ROCK"){
         if(hU =="PAPER"){
-            console.log(`You win, your ${hChoice} convers the computer's ${cChoice}`);
+            console.log(`You won this time your ${hChoice} covers my ${cChoice}`);
+            return "Human";
             done = true;
         }
         else if(hU =="SCISSOR"){
-            console.log(`You lose, your ${hChoice} is broken by the computer's ${cChoice}`);
+            console.log(`I won this time my ${cChoice} breaks your ${hChoice}`);
+            return "Computer";
             done = true;
         }
     }
     else if (cU =="PAPER"){
         if(hU == "SCISSOR"){
-            console.log(`You win, your ${hChoice} cuts the computer's ${cChoice}`);
+            console.log(`You won this time your ${hChoice} cuts my ${cChoice}`);
+            return "Human";
             done = true;
         }
         else if(hU =="ROCK"){
-            console.log(`You lose, your ${hChoice} is covered by the computer's ${cChoice}`);
+            console.log(`I won this time my ${cChoice} covers your ${hChoice}`);
+            return "Computer";
             done = true;
         }
     }
     else if (cU == "SCISSOR"){
         if(hU =="ROCK"){
-            console.log(`You win, your ${hChoice} breaks the computer's ${cChoice}`);
+            console.log(`You won this time your ${hChoice} breaks my ${cChoice}`);
+            return "Human";
             done = true;
         }
         else if(hU == "PAPER"){
-            console.log(`You lose, your ${hChoice} is cut by the computer's ${cChoice}`);
+            console.log(`I won this time my ${cChoice} cuts your ${hChoice}`);
+            return "Computer";
             done = true;
         }
         
@@ -54,11 +58,40 @@ function playerSelection(hChoice, cChoice){
         done = true;
     }
     if(hU === cU){
-        alert("Please try again your choice you choose the same as the computer");
+        console.log("Its a Draw!!!");
         done = true;
     }
     
 }
 
+function game(times){
+    let computer = 0; 
+    let human = 0;
+    time = Number(times);
+    for(let i=0; i<times; i++){
+        //Geting users choice
+        let hChoice = prompt("Please enter your choice");
+        let cChoice = getComputerChoice();
+        let result = playerRound(hChoice, cChoice);
+        if(result == "Human"){
+            human++;
+        }
+        else if(result == "Computer"){
+            computer++;
+        }
+    }
+    console.log("\n\n\n------------------------------\n\n\n")
+    console.log(`The computer won ${computer} times`);
+    console.log(`And the human won ${human} times`);
+    if(computer>human){
+        console.log("Harrah the Computer Won!!!!");
+    }
+    else if(human>computer){
+        console.log("You did it!!! the computer is beat!!!!");
 
-playerSelection(hChoice, cChoice);
+    }
+    else{
+        console.log("What'd yah know its a draw!!! \n not the draw of a place, but just could not beat me ;D");
+    }
+}
+game(prompt("how many rounds would you like to play?"))
