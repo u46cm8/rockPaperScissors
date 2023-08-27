d6d53e2329516f859b5f2473655ec326da409732
u46cm8 <touch2who@gmail.com>
2023-08-27 22:26:58 +0100

    Everyting is working only need to finish the game conclusiond logic and comparisons of choice

--- JaSc.js
+++ JaSc.js
@@ -1,10 +1,37 @@
 
-const choices = ["Rock", "Paper", "Scissors"];
+const choices = ["rock", "paper", "scissors"];
+
+var compSelection; 
+var humanSelection;
 
 function getComputerChoice(){
     //getting a random number out of four to make a choice from the list
-    var compChoice = Math.floor(Math.random()*5);
+    var Selection = Math.floor(Math.random()*3);
 
-    console.log(choices[compChoice])
+    console.log(choices[Selection]);
+     return Selection;
     
+}
+
+//Testing to see if it works
+//getComputerChoice();
+
+function getHumanChoice(){
+    var Selection = document.getElementById("RockPaperScissors").value.toString();
+//Testing out the inputs
+    console.log(Selection.toLowerCase());
+    return Selection;
+
+}
+
+
+function game(compSelection)  {
+    
+    getComputerChoice();
+    humanSelection = getHumanChoice().toLowerCase();
+
+    if(humanSelection == "rock"){
+        console.log("it works");
+    }
+
 }
